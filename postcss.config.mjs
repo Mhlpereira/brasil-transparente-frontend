f715a555d0ab7e73b53db56843d4e925157848a4
Mario Henrique <mariohenriquelp@gmail.com>
2025-06-01 17:41:18 -0300

    arrumado incompatibilidade do tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss", "autoprefixer"],
-};
-
-export default config;
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+}
